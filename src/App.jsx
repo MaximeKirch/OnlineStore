eae78c6c6d1792d0808c7b0d08b6b07fa138f3ac
ausma-tc <t.corbieres@gmail.com>
2023-02-17 20:03:35 +0100

    Fix firebase connection and add form & listing element from firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,16 @@
 import React, { useState } from "react";
 import { Container, Text, Button } from "@chakra-ui/react";
-import { getDb } from "./firebase.js"
+// import { getDb } from "./firebase.js"
 //import {collection, getDocs} from 'firebase/firestore'
+import Todo from './components/Todo'
 
 function App() {
-  const [data, setData] = useState(null);
-
-  //const products = database.collection('products')
-
-  // const getData = async () => {
-  //     await getDocs(collection(getDb(),"products"))
-  //       .then((querySnapshot) => {
-  //         const newData = querySnapshot.docs
-  //           .map((doc) => ({...doc.data(), id:doc.id }))
-  //           setData(newData)
-  //           console.log(newData)
-  //       })
-  // };
 
   return (
     <Container maxW="100%">
       <Text>Hello World</Text>
       <Button>Click</Button>
+      <Todo />
     </Container>
   );
 }
