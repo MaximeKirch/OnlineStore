bc8074452472d9d0038d812f3b7508af152d4692
Maxime Kirch <maxime.kirch@gmail.com>
2023-02-17 17:41:39 +0100

    feature : firebase init + connection

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,29 @@
-import React, { useState } from 'react'
-import {Container, Text} from '@chakra-ui/react'
+import React, { useState } from "react";
+import { Container, Text, Button } from "@chakra-ui/react";
+import { getDb } from "./firebase.js"
+//import {collection, getDocs} from 'firebase/firestore'
 
 function App() {
-  
-return (
+  const [data, setData] = useState(null);
+
+  //const products = database.collection('products')
+
+  // const getData = async () => {
+  //     await getDocs(collection(getDb(),"products"))
+  //       .then((querySnapshot) => {
+  //         const newData = querySnapshot.docs
+  //           .map((doc) => ({...doc.data(), id:doc.id }))
+  //           setData(newData)
+  //           console.log(newData)
+  //       })
+  // };
+
+  return (
     <Container maxW="100%">
-      <Text>Hello World</Text> 
+      <Text>Hello World</Text>
+      <Button>Click</Button>
     </Container>
-)
+  );
 }
 
-export default App
+export default App;
